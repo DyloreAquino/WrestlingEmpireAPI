b9e6b4aeb03f0c283f661f2c4c7e57207eb853ec
DyloreAquino <lutherjerold@gmail.com>
2026-05-31 09:23:05 +0800

    Organized api routes

--- wrestling-empire-api/routes/api.php
+++ wrestling-empire-api/routes/api.php
@@ -18,14 +18,20 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function() {
+    // Routes with CRUD features
     Route::apiResource('wrestlers', WrestlerController::class);
     Route::apiResource('championships', ChampionshipController::class);
     Route::apiResource('events', EventController::class);
-    Route::apiResource('match_types', MatchTypeController::class);
-    Route::apiResource('promotions', PromotionController::class);
     Route::apiResource('shows', ShowController::class);
-    Route::apiResource('stipulations', StipulationController::class);
     Route::apiResource('teams', TeamController::class);
-    Route::apiResource('territories', TerritoryController::class);
     Route::apiResource('title_reigns', TitleReignController::class);
+
+    // Routes that are read-only
+    Route::apiResource('match_types', MatchTypeController::class)->only(['index', 'show']);
+    Route::apiResource('promotions', PromotionController::class)->only(['index', 'show']);
+    Route::apiResource('stipulations', StipulationController::class)->only(['index', 'show']);
+    Route::apiResource('territories', TerritoryController::class)->only(['index', 'show']);
+    
+    // Special routes for special functionality
+    
 });
